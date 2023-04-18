f5b6f3dc757d8cce1725a504ceb1952a44841e3f
Bryan-ro <bryangomesrocha@gmail.com>
2023-04-17 23:37:08 -0300

    user validations and .eslint

---
+++ src/controllers/userControllers/UserController.ts
@@ -0,0 +1,9 @@
+import  { User } from "../../services/User";
+import { Request, Response, Router } from "express";
+const router = Router();
+
+class UserController {
+    private _login(req: Request, res: Response) {
+
+    }
+}
